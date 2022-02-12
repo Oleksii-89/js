5bf3b37737d76713fb2aff1090f72a955044f870
Alex Feshchenko <prodaj197@gmail.com>
2022-02-12 09:42:59 +0100

    first folder

---
+++ new/js/script.js
@@ -0,0 +1,16 @@
+/*jshint esversion: 6 */
+
+// alert("hello");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("vam est 18?", "10");
+// console.log(answer + 5);
+
+const answer = [];
+answer [0] = prompt('Kak imia?', '');
+answer [1] = prompt('Skolko let, epta?', '');
+answer [2] = prompt('Lulakebab?', '');
+
+document.write(answer);
